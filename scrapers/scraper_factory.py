98315914cddefb24f3ccc19a47e272f9b70a64dc
k <j>
2024-09-03 19:28:45 -0600

    added three new sources

--- scrapers/scraper_factory.py
+++ scrapers/scraper_factory.py
@@ -4,7 +4,7 @@ from scrapers.msg_scraper import MSGScraper
 
 
 def get_scraper(source: Source, db_handler: DBHandler):
-    if source.venue == "Madison Square Garden":
+    if source.venue in ["Madison Square Garden", "Theater at MSG", "Beacon Theater", "Radio City Music Hall"]:
         return MSGScraper(source, db_handler)
     # Add more conditions for other venues
     else:
